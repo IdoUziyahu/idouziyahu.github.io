20f03baf6c7fe369b1dda12d2bfe1c6e7efbec16
Ido Uziyahu <idouziyahu2002@gmail.com>
2022-12-03 19:20:08 +0200

    first design of the webpage

---
+++ assets/js/indexAfterLoad.js
@@ -0,0 +1,22 @@
+indexAfterLoad = function(){
+
+    //set age
+    let setAge = function(){
+        let ageSpan = $("#age")
+
+        let birthday = new Date(ageSpan.attr("data-birthday"))
+        let today = new Date();
+
+        //calcs the year difference
+        let age = today.getFullYear() - birthday.getFullYear()
+
+        //if i hadn't have birthday this year, remove one year
+        birthday.setFullYear(today.getFullYear())
+        if(birthday > today) age -= 1;
+
+        ageSpan.text(age);
+    }
+    setAge()
+
+}
+indexAfterLoad()
